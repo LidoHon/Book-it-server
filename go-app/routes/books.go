91c4064873afe8cd4deddb05a6ab6f8c8216fe82
LidoHon <lidohon10@gmail.com>
2025-01-17 12:10:35 +0300

    add image upload middleware to inserting book controller in routes

--- go-app/routes/books.go
+++ go-app/routes/books.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	"github.com/LidoHon/devConnect/controllers"
+	"github.com/LidoHon/devConnect/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func BooksRoutes(incomingRoutes *gin.Engine) {
 	booksRoutes := incomingRoutes.Group("/api/books")
 	{
-		booksRoutes.POST("/insert", controllers.InsertBooks())
+		booksRoutes.POST("/insert",middlewares.ImageUpload(), controllers.InsertBooks())
 		booksRoutes.GET("/get-books", controllers.GetBooks())
 		booksRoutes.GET("/get-books/:id", controllers.GetBooksById())
-		booksRoutes.PUT("/update-books/:id", controllers.UpdateBooks())
+		booksRoutes.PUT("/update-books/:id", middlewares.ImageUpload(), controllers.UpdateBooks())
 		booksRoutes.DELETE("/delete-books/:id", controllers.DeleteBooks())
 	}
 }
